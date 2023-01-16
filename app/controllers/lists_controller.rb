e5a35c79439310d4873aee3a2b4aa61e862c870f
chelsea roberts <chelsea.j.roberts@hotmail.com>
2023-01-16 18:18:19 +0000

    stimulus added

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,29 +1,42 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show]
+   before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def new
-     @list = List.new(params[:list])
-  end
-
-  def create
-    @list = List.new(list_params)
-    @list.save
-    redirect_to lists_path(@list)
+     @list = List.new
+    #  (params[:list])
   end
 
   def show
     @bookmark = Bookmark.new()
+    # @review =Review.new()
   end
+
+  def create
+    @list = List.new(list_params)
+     if @list.save
+    redirect_to lists_path(@list)
+     else
+      render :new, status: :unprocessable_entity
+     end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path(@list)
+  end
+
+  private
 
   def set_list
     @list = List.find(params[:id])
   end
 
   def list_params
-  params.require(:list).permit(:name)
-end
+  params.require(:list).permit(:name, :photo)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  # get 'lists/only:'
-  # get 'lists/:index,'
-  # get 'lists/:new,'
-  # get 'lists/:create,'
-  # get 'lists/:show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root to: "lists#index"
 
   resources :lists, only: [:index, :new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
   end
+  resources :lists, only: [:destroy]
   resources :bookmarks, only: [:destroy]
 end
