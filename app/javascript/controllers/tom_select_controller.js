3238916f342d206721adc3df66c1e5cf5d5bfb74
chelsea roberts <chelsea.j.roberts@hotmail.com>
2023-01-18 12:05:11 +0000

    Need to fix API

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -1,8 +1,22 @@
+// import { Controller } from "@hotwired/stimulus"
+// import TomSelect from "tom-select"
+
+// export default class extends Controller {
+//   connect() {
+//     new TomSelect(this.element, {})
+//   }
+// }
+
 import { Controller } from "@hotwired/stimulus"
-import TomSelect from "tom-select"
+import TomSelect from "tom-select";
 
 export default class extends Controller {
+  // ...
+
   connect() {
-    new TomSelect(this.element, {})
+    new TomSelect(
+      this.element ,
+      this.optionsValue
+    );
   }
 }
